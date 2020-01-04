b97678cb0544537c7d340ed2be895308c5301eef
jason-toups <me@jasontoups.com>
2020-01-03 16:28:25 -0800

    gameboard array generating, and JS classes added for Square and Game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
 console.log('got some js!');
+
+class Square {
+  constructor(color, divClass) {
+    this.color = this.applyRandomColor();
+    this.divClass = '<div class="square"/>';
+  }
+  applyRandomColor() {
+    const colors = ['cornflowerblue', 'cornflowerblue', 'cornflowerblue', 'darkseagreen', 'turquoise', 'violet', 'gold', 'tomato'];
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index];
+  }
+}
+
+class Game {
+  constructor() {
+    this.score = 0;
+    this.matchGoal = "";
+  }
+}
+
+$('button').on('click', () => {
+  console.log('Game Start');
+  gameStart();
+  // setTimer();
+});
+
+const generateGameBoardArray = (width, height) => {
+  // to generate the gameboard array
+  // take the width passed as an argument
+  let gameBoardArray = [];
+  // loop through the width
+  for (let i = 0; i < width; i++) {
+    // The width will be the x limit
+    // console.log(`width = ${i}`);
+    let gameBoardArrayHeight = [];
+    for (let j = 0; j < height; j++) {
+      // within each x index, there should be a height length array pushed to each x index.
+      // console.log(`height = ${j}`);
+      gameBoardArrayHeight.push(new Square);
+    }
+    gameBoardArray.push(gameBoardArrayHeight);
+  }
+  console.log(gameBoardArray);
+  return gameBoardArray;
+}
+
+
+const gameStart = () => {
+  generateGameBoardArray(5, 4);
+}
+
+// console.log(gameBoardArray);
