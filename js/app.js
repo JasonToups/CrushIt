1c8ec911aa9227235bba3fd70cd52cce6c4d9dc2
jason-toups <me@jasontoups.com>
2020-01-06 17:00:53 -0800

    removed redundant code, refactored validateMatch(), will be working on recursive validation next

--- js/app.js
+++ js/app.js
@@ -18,111 +18,59 @@ $('button').on('click', () => {
   setTimer();
 });
 
+
 const handlePoke = (event) => {
   console.log(event.target);
-  const color = $(event.target).css('background-color');
   $(event.target).addClass('poked').css('opacity', 0.3);
   console.log($(event.target).index());
-  compareColors(color, game.goalColor);
+  console.log(validateMatch(event.target, $('.goal-square')));
+  return validateMatch(event.target, $('.goal-square'));
 }
-
-// Accepts two parameters to compare colors.
-const compareColors = (color, validate) => {
-  const colorValues = color.substring(4, color.length - 1).split(', ');
-  const validateValues = validate.substring(4, validate.length - 1).split(', ');
-  if (colorValues[0] === validateValues[0] && colorValues[1] === validateValues[1] && colorValues[2] === validateValues[2]) {
-    console.log('got the same color!');
-    // I don't think I need the game score incrementing here.
-    game.score++;
-    $('#scoreNumber').text(`${game.score}`);
-    return true;
-  } else {
-    score--
-    return false;
-  }
-};
 
 $('.squares').on('click', '.square', handlePoke);
 
 /* Write Recursive function that validates the colors in all cardinal directions of the current square, and returns an array of matchable items to the game object in game.currentMatch1 and game.currentMatch2 */
 
-// This works if I pass in the JQuery index of the .square array
 
 // If I can navigate up one .row and down one .row at the same index, I can compare the squares to validate the match
+
 const validateMatch = (square1, square2) => {
+  console.log('validate match function');
   const current = $(square1).css('background-color');
   const validate = $(square2).css('background-color');
-  return compareColors(current, validate);
+  console.log(current);
+  console.log(validate);
+  if (current === validate) {
+    game.score++;
+    $('#scoreNumber').text(`${game.score}`);
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
-
 
 const applyRandomColor = () => {
   const colors = ['rgb(100, 149, 237)', 'rgb(143, 188, 143)', 'rgb(64, 224, 208)', 'rgb(238, 130, 238)', 'rgb(255, 215, 0)', 'rgb(255, 99, 71)'];
   const index = Math.floor(Math.random() * colors.length)
   return colors[index];
 }
-
-/* This creates the Divs for the gameboard */
-/*  This function pretty functional, but not working as intended.  */
-// const generateGameBoard = () => {
-//   // to put the gameboard on the DOM
-//   // loop through the gameboard
-//   const $squares = $('.squares');
-//   // const $square = $('<div class="square"/>');
-//   const $row = $('<div class="row"/>');
-//   for (let i = 0; i < game.boardWidth; i++) {
-//     // console.log('row dom loop working')
-//     // // for each index in the gameboard loop through the array
-//     const $square = $('<div class="square"/>');
-//     $square.css('background-color', applyRandomColor());
-//     // $row.append($square.clone());
-//     $row.append($square);
-//     // $row.append($square);
-//   }
-
-//   for (let j = 0; j < game.boardHeight; j++) {
-//     // console.log('column dom loop working')
-//     $squares.append($row.clone());
-//     // $squares.append($row);
-//     // // // at each array in the gameboard index, create a div in the div squares class
-//   }
-// }
-
-/* I need to refactor this code to where the row is being created first, and then loop within the row to create the squares */
+/* ------- Creating the Game Board ------- */
 const generateGameBoard = () => {
-  // to put the gameboard on the DOM
-  // loop through the gameboard
   const $squares = $('.squares');
-  // const $square = $('<div class="square"/>');
-
   for (let i = 0; i < game.boardHeight; i++) {
     const $row = $(`<div class="row" id=${i}/>`);
-    console.log('appending a row')
+    // console.log('appending a row');
     for (let j = 0; j < game.boardWidth; j++) {
-      // console.log('column dom loop working')
+      // console.log('appending a square');
       const $square = $('<div class="square"/>');
       $square.css('background-color', applyRandomColor());
-      // $row.append($square.clone());
       $row.append($square);
     }
     $squares.append($row);
   }
-
-  // for (let i = 0; i < game.boardWidth; i++) {
-  //   // console.log('row dom loop working')
-  //   // // for each index in the gameboard loop through the array
-  //   const $square = $('<div class="square"/>');
-  //   $square.css('background-color', applyRandomColor());
-  //   // $row.append($square.clone());
-  //   $row.append($square);
-  //   // $row.append($square);
-  // }
-
 }
 
-/* This randomizes the .square colors */
+/* This randomizes all .square colors */
 const updateSquareColors = () => {
   $('.square').each(function () {
     $(this).css('background-color', applyRandomColor());
@@ -155,4 +103,3 @@ const gameStart = () => {
   game.goalColor = color;
   $('.goal-square').css('background-color', color);
 }
-
