8eaf5f00c1cc657a2f8b0ec04860ec622b1aca95
Jason Toups <me@jasontoups.com>
2020-01-07 17:49:56 -0800

    removed comments and unnecessary code

--- js/app.js
+++ js/app.js
@@ -21,77 +21,44 @@ const handlePoke = (event) => {
   console.log(event.target);
   $(event.target).addClass('selected');
   $(event.target).addClass('animated pulse infinite');
-  /* Finding Current Index of Poke and Parent row ID */
-  // let parent = parseInt($(event.target).parent().attr('id'));
-  // console.log(parent);
-  // console.log(typeof parent);
-  // let currentIndex = $(event.target).index();
-  // console.log(currentIndex);
-  // console.log(typeof currentIndex);
-
+  
   /* Sending event target to game object */
   game.matchArray.push(event.target);
   console.log(validateMatch(event.target, $('.goal-square')));
   validateMatchArray();
   return validateMatch(event.target, $('.goal-square'));
-}
+};
 
 $('.squares').on('click', '.square', handlePoke);
 
 /* Write Recursive function that validates the colors in all cardinal directions of the current square, and returns an array of matchable items to the game object in game.currentMatch1 and game.currentMatch2 */
 
 
-// If I can navigate up one .row and down one .row at the same index, I can compare the squares to validate the match
+//This function should accept 2 parameters, so i can pass in game.matchArray1, game.matchArray2, and pass in the class name i plan on appending to the div to control the recursive function, .match1, .match2
 const validateMatchArray = () => {
   console.log('Validate Match Array Function Start');
   let matchArray = game.matchArray;
-  /*  This is how this for loop should start */
+  
   for (let i = 0; i <= matchArray.length; i++) {
-
-    /* DEBUGGING FOR LOOP */
-    // for (let i = 0; i < 20; i++) {
-    /* Finding Current Index of Poke and Parent row ID */
-    console.log(`Match Array index ${i}`)
-    // let parent = parseInt($(matchArray).eq(i).parent().attr('id'));
+    console.log(`Match Array index ${i}`);
     let parent = parseInt($(matchArray[i]).parent().attr('id'));
     let currentIndex = $(matchArray[i]).index();
     console.log(`row ${parent}`);
-    // console.log(typeof parent);
     console.log(`column ${currentIndex}`);
-    // let currentSquare = $(matchArray).eq(i);
+    
+    /* Getting the neighboring squares in 4 cardinal directions */
     let currentSquare = matchArray[i];
-    // console.log(typeof currentIndex);
-    /* Getting the squares in 4 cardinal directions from the current index */
-    /* somewhat working code */
-    // let squareAbove = $(`#${parent > 0 ? parent - 1 : parent}`).children().eq(currentIndex);
-    // let squareBelow = $(`#${parent <= game.boardHeight ? parent + 1 : parent}`).children().eq(currentIndex);
     let squareAbove = $(`#${parent > 0 ? parent - 1 : false}`).children().eq(currentIndex);
     let squareBelow = $(`#${parent < game.boardHeight ? parent + 1 : false}`).children().eq(currentIndex);
-    // let squareLeft = $(`#${parent}`).children().eq(currentIndex > 0 ? currentIndex : false).prev();
     let squareLeft = currentIndex > 0 ? $(`#${parent}`).children().eq(currentIndex).prev() : false;
     let squareRight = currentIndex < game.boardWidth ? $(`#${parent}`).children().eq(currentIndex).next() : false;
-    // console.log('currentSquare');
-    // console.log(currentSquare);
-    // console.log('squareAbove');
-    // console.log(squareAbove);
-    // console.log('squareBelow');
-    // console.log(squareBelow);
-    // console.log('squareLeft');
-    // console.log(squareLeft);
-    // console.log('squareRight');
-    // console.log(squareRight);
-
+    
     /* Getting the background colors from the squares in 4 cardinal directions from the current index  */
     let currentSquareColor = ($(currentSquare).css('background-color'));
     let squareAboveColor = ($(squareAbove).css('background-color'));
     let squareBelowColor = ($(squareBelow).css('background-color'));
     let squareLeftColor = ($(squareLeft).css('background-color'));
     let squareRightColor = ($(squareRight).css('background-color'));
-    // console.log(`currentSquareColor = ${currentSquareColor}`);
-    // console.log(`squareAboveColor = ${squareAboveColor}`);
-    // console.log(`squareBelowColor = ${squareBelowColor}`);
-    // console.log(`squareLeftColor = ${squareLeftColor}`);
-    // console.log(`squareRightColor = ${squareRightColor}`);
 
     /* ----- Control Flow for Pushing Square Divs to Game Match Array ----- */
     /* write includes() where it checks the game.matchArray and if the current div is already included, then skip it. 
@@ -152,6 +119,7 @@ const validateMatch = (square1, square2) => {
   }
 };
 
+/* -- this will apply a random color from the color array -- */
 const applyRandomColor = () => {
   /* ---- This is the array of colors I want to use in the game */
   // const colors = ['rgb(100, 149, 237)', 'rgb(143, 188, 143)', 'rgb(64, 224, 208)', 'rgb(238, 130, 238)', 'rgb(255, 215, 0)', 'rgb(255, 99, 71)'];
@@ -204,7 +172,6 @@ const updateTime = () => {
 
 const gameStart = () => {
   generateGameBoard();
-  // updateSquareColors();
   let color = applyRandomColor()
   game.goalColor = color;
   $('.goal-square').css('background-color', color);
